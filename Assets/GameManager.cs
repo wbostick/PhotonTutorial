e8894b5871fc17bea56d67411f570ae568d05cef
billy <wbostick@umich.edu>
2020-12-12 22:43:45 -0500

    add quit button

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -26,7 +26,7 @@ namespace Com.Billy.PhotonTutorial
         {
             SceneManager.LoadScene(0);
         }
-        
+
         public override void OnPlayerEnteredRoom(Player other)
         {
             Debug.LogFormat("OnPlayerEnteredRoom() {0}", other.NickName); // not seen if you're the player connecting
